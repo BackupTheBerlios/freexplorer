668f3259f428410a509e983b7267d1c99c1d7c6a
blackfox <blackfox>
2006-04-14 23:07:36 +0000

    Dossiers de configurations dépendants de l'OS.

--- FreeXplorer/FreeXplorer/FreeXplorer.cs
+++ FreeXplorer/FreeXplorer/FreeXplorer.cs
@@ -11,8 +11,18 @@ namespace Wizou.FreeXplorer
         {
             get
             {
-                return Path.Combine(Environment.GetFolderPath(
-                    Environment.SpecialFolder.ApplicationData), "FreeXplorer");
+                // http://www.mono-project.com/FAQ:_Technical#How_to_detect_the_execution_platform_.3F
+                int platform = (int) Environment.OSVersion.Platform;
+                if ((platform == 4) || (platform == 128))
+                {
+                    return Path.Combine(Environment.GetFolderPath(
+                        Environment.SpecialFolder.Personal), ".freexplorer");
+                }
+                else
+                {
+                    return Path.Combine(Environment.GetFolderPath(
+                        Environment.SpecialFolder.ApplicationData), "FreeXplorer");
+                }
             }
         }
     }
